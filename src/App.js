7c3d583388e1d9728cd643dd77ac6575a4ac917b
nedmorgan <nmorgan1987@gmail.com>
2020-05-12 16:12:00 -0400

    updating deployment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Testing
+          Testing Make File
         </a>
       </header>
     </div>
