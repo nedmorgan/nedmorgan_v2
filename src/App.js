dae7d45ad4d4fa0f703477dacee866c1682d5028
Nathaniel Morgan <natmorgan@coca-cola.com>
2020-05-13 17:23:01 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import NavBar from '../src/components/NavBar'
 import Logo from '../src/components/Logo'
 
-function App() {
-  return (
-    <div className='App'>
-      <NavBar />
-      <Logo />
-    </div>
-  )
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-export default App
+  render() {
+    return (
+      <div className='App'>
+        <NavBar />
+        <Logo />
+      </div>
+    )
+  }
+}
